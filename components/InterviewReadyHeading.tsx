121f3d7da387dc712a13d48c2bbfb6e80bc7c23f
Sebastian Duque <sebastian.duque@intercom.io>
2025-04-14 13:49:47 -0700

    Starter version

---
+++ components/InterviewReadyHeading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function InterviewReadyHeading() {
+  return (
+    <h1 className="text-4xl font-bold flex flex-row items-center gap-2">
+      <img src="/fin-talking.gif" alt="Fin" className="w-10 h-10" />
+      Hi, youߴre ready for your interview
+    </h1>
+  );
+}
